38899993e37ff45facfb01c98b4c45da4251cbd9
brickstar <brickstar@gmail.com>
2018-07-16 12:01:08 -0600

    authorization

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @user = User.find_by_username(params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      # TODO: FIXME: flash message username/password combo wasn't valid
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
